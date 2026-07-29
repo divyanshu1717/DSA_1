8e1a69686e1bbc29d745b8451c17a092a6884be6
divyanshu1717 <divyanshu90450@gmail.com>
2026-07-30 00:51:50 +0530

    Add DSA_1 code

---
+++ Sumofdigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Sumofdigits {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+  System.out.println("enter the number : ");
+        int n = sc.nextInt();
+        int rev = 0;
+
+        while(n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+
+        System.out.println("Reverse = " + rev);
+    }
+}
